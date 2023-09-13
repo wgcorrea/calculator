433fb1b9f5886630dd32be8c593c26b09214ebdb
Willian Correa <15524575+wgcorrea@users.noreply.github.com>
2023-09-12 23:12:08 -0400

    refactor to remove unecessary code

--- mathextra/src/lib/fraction.ts
+++ mathextra/src/lib/fraction.ts
@@ -6,7 +6,6 @@ export type Fraction = {
 export const fraction = (numerator: number, denominator = 1): Fraction => {
   return {
     plus: (other: Fraction) => {
-      if (numerator === 0) return other;
       return fraction(numerator + other.toInteger(), denominator);
     },
     toInteger: () => numerator,
